a8a26bbaf99d31fb3ce7b2f8ac5380ce486d5064
MateuszChmielewski99 <chmielewski079@gmail.com>
2020-11-25 21:37:41 +0100

    added healthcheck endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import { bootstrap } from './container-setup';
 import CountryRouter from './controllers/Country/CountryController';
 import LanguageRouter from './controllers/Langauge/LangaugeController';
@@ -10,6 +10,10 @@ const app: Application = express();
 
 app.use('/country', CountryRouter);
 app.use('/language', LanguageRouter);
+
+app.get('/',(_req:Request, res:Response) => {
+  return res.sendStatus(200);
+})
 
 const port = process.env.PORT || 6000;
 app.listen(port, () => {
