2315e20a7265e455f7bc89044e4b7979604ff000
MateuszChmielewski99 <chmielewski079@gmail.com>
2020-12-01 22:45:29 +0100

    added cors

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import express, { Application, Request, Response } from 'express';
 import { bootstrap } from './container-setup';
 import CountryRouter from './controllers/Country/CountryController';
 import LanguageRouter from './controllers/Langauge/LangaugeController';
+import cors from 'cors';
 
 bootstrap();
 
 const app: Application = express();
+
+app.use(cors({ origin: '*' }));
 
 app.use('/country', CountryRouter);
 app.use('/language', LanguageRouter);
